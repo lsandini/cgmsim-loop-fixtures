94e73bfae1a6e96efb8f53231ff1e7068c79151b
Lorenzo <lsandini@gmail.com>
2026-06-09 20:05:31 +0300

    ci: bump actions/checkout to v5 (Node 24)

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add .
-git commit -m "harness: add delayMinutes + §3.7 and peak-75 dosing oracle scenarios"
+git commit -m "ci: bump actions/checkout to v5 (Node 24)"
 git push -u origin main
